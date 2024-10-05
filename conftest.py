10297f39ce56ceb677c4cb3aee520cf1ed0f22a5
k.zolotovsky <k.zolotovsky@gazpromcps.ru>
2024-10-05 21:19:29 +0700

    init commit

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(autouse=True)
+def browser_config():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open("https://www.google.com/ncr")
+
+    yield
+
+    browser.quit()
---
+++ test_search.py
@@ -0,0 +1,10 @@
+from selene import browser, be, have
+
+
+def test_positive_search():
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+def test_negative_search():
+    browser.element('[name="q"]').should(be.blank).type('hsdfgsdgsdgfsdgfsdgfsdfgsdgfsdgsdg').press_enter()
+    browser.element('.card-section').should(have.text("Your search - hsdfgsdgsdgfsdgfsdgfsdfgsdgfsdgsdg - did not match any documents."))
